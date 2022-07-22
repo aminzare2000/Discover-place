e50f3bd66255a7486d50cde5dd1d789ab2f44502
am zare <amin.zare@gmail.com>
2022-07-22 15:37:57 +0430

    call venues and show in index.htm

--- calltest.py
+++ calltest.py
@@ -1,8 +1,36 @@
 from OpenAlexTools import OpenAlexTools
+from json import *
 
-oaTools = OpenAlexTools()
-# 2167-8359
-pages_of_venues = oaTools.get_venues('0029-3970') 
-for page in pages_of_venues:
-    for work in page['results']:
-        print(work['display_name'])
+def get_journal_by_issn(issn):
+    oaTools = OpenAlexTools()
+    # 0029-3970
+    pages_of_venues = oaTools.get_venues('2167-8359') 
+    for page in pages_of_venues:
+        for work in page['results']:
+            return work
+            # display_name: "PeerJ",
+            # publisher: "PeerJ",
+            # works_count: 21100,
+            # cited_by_count: 151312,
+            # is_oa: true,
+            # is_in_doaj: true,
+            # homepage_url: "http://www.peerj.com/",        
+            # print(work['issn']) # list of issns
+            # print(work['display_name'])
+            # print(work['publisher'])
+            # print(work['works_count'])
+            # print(work['cited_by_count'])
+            # print(work['is_oa'])
+            # print(work['is_in_doaj'])
+            # print(work['homepage_url'])
+            # print(work['created_date']) # date of creation
+            
+            counts_by_year_list = work['counts_by_year']
+            for item in counts_by_year_list:
+                print(item['year'])
+                print(item['works_count'])
+                print(item['cited_by_count'])
+
+        
+        
+
--- routes.py
+++ routes.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from calltest import get_journal_by_issn
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return render_template('index.html')
+    data = get_journal_by_issn('0029-3970')
+    return render_template('index.html', data = data)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
